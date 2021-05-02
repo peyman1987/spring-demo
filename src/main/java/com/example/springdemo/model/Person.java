73c5fff0a5eb396153b18609373d2a810d1e5061
ISC024\ISC_024 <peyman_em@yahoo.com>
2021-05-02 19:39:42 +0200

    Unit test API

---
+++ src/main/java/com/example/springdemo/model/Person.java
@@ -0,0 +1,26 @@
+package com.example.springdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+
+@Getter
+public class Person {
+
+    private final UUID id;
+
+    @NotBlank
+    private final String name;
+
+
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
